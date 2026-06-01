63830b6a075d8687957946265762967c28cec526
Lim Woojae <limwoojae.dev@gmail.com>
2026-06-01 20:32:21 +1000

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> hash_map;
+        vector<vector<string>> ans;
+
+        for (string str : strs) 
+        {
+            vector <int> arr(26);
+
+            for (char c: str)
+                arr[c - 'a']++;    
+            
+            string key_string = "";
+            for (int n : arr)
+                key_string += to_string(n) + "#";
+
+            hash_map[key_string].push_back(str);
+        }
+
+        for (const auto& [key, val] : hash_map)
+        {
+            ans.push_back(val);
+        }
+
+        return ans;
+    }
+};
