5338e50340ead70fcaa6f7bf0f7023f7b621a917
Lim Woojae <limwoojae.dev@gmail.com>
2026-06-01 23:16:29 +1000

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> ans;
+        int total = 1;
+
+        for (int num : nums)
+            total *= num;
+
+        std::cout << total;
+
+        for (int i {0}; i < nums.size(); ++i)
+        {
+            if (nums[i] == 0)
+            {
+                total = 1;
+
+                for (int j {0}; j < nums.size(); ++j)
+                {
+                    if (i != j)
+                        total *=  nums[j];
+                }
+                ans.push_back(total);
+                total = 0;
+            }
+            else
+                ans.push_back(total / nums[i]);
+        }
+
+        return ans;
+    }
+};
