fe1f1251a67ed26d248dc78fe66dafa9fd54a872
Lim Woojae <limwoojae.dev@gmail.com>
2026-06-02 00:01:26 +1000

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_map<int, unordered_set<char>> square_map;
+        unordered_map<int, unordered_set<char>> row_map;
+        unordered_map<int, unordered_set<char>> col_map;
+        
+        for (int i = 0; i < board.size(); ++i) {
+            for (int j = 0; j < board[i].size(); ++j) {
+                int square_idx = (i / 3) * 3 + (j / 3);
+
+                if (isdigit(board[i][j])) {
+                    if (!square_map[square_idx].insert(board[i][j]).second ||
+                        !row_map[i].insert(board[i][j]).second ||
+                        !col_map[j].insert(board[i][j]).second) 
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+};
