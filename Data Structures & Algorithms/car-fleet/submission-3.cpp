e52bb293773ae676b664456dedae32016a7d6bf1
Lim Woojae <limwoojae.dev@gmail.com>
2026-06-02 13:43:25 +1000

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int, int>> cars;
+        stack<double> st;
+
+        for (int i = 0; i < position.size(); ++i) 
+            cars.push_back({position[i], speed[i]});
+
+        sort(cars.begin(), cars.end(), greater<pair<int, int>>());
+
+        for (auto &p : cars) {
+            double time = (static_cast<double>(target - p.first) / p.second);
+
+            if (st.empty() || st.top() < time)
+                st.push(time);
+        }
+
+        return st.size();
+    }
+};
