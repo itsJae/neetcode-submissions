9562231437af2dbe124107decd7c64f1e2afc30e
Lim Woojae <limwoojae.dev@gmail.com>
2026-06-04 08:45:41 +1000

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for (int i = 0; i < matrix.size(); ++i) {
+            int left = 0;
+            int right = matrix[i].size() - 1;
+
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                
+                if (matrix[i][mid] == target) {
+                    return true;
+                } else if (matrix[i][mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+};
