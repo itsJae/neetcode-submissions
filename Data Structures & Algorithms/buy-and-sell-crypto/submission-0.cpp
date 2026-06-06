465b36283f49d01fddf2938d286e442731d6d513
Lim Woojae <limwoojae.dev@gmail.com>
2026-06-06 15:16:17 +1000

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max_profit = 0;
+        int min_buy = prices[0];
+
+        for (int i = 0; i < prices.size(); ++i) {
+            if (prices[i] - min_buy > max_profit) {
+                max_profit = prices[i] - min_buy;
+            }
+
+            if (min_buy > prices[i]) {
+                min_buy = prices[i];
+            }
+        }
+
+        return max_profit;
+    }
+};
