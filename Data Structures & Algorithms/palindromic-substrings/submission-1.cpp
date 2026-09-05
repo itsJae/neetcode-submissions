9705be1ff5a123f6d87bb5970169ef7f6349547b
Lim Woojae <limwoojae.dev@gmail.com>
2026-09-05 21:29:52 +1000

    Add: palindromic-substrings - submission-1

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int cnt = 0;
+        int n = s.size();
+
+        for (int i = 0; i < n; ++i) {
+            int l = i;
+            int r = i;
+
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                ++cnt;
+                --l;
+                ++r;
+            }
+
+            l = i;
+            r = i + 1;
+
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                ++cnt;
+                --l;
+                ++r;
+            }
+        }
+
+        return cnt;
+    }
+};
