c0571571066e729b3f1ce9c07f94c3484c11a8be
Lim Woojae <limwoojae.dev@gmail.com>
2026-09-06 23:00:32 +1000

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int targetIdx = n - 1;
+
+        for (int i = n - 2; i >= 0; --i) {
+            if (targetIdx - i <= nums[i]) 
+                targetIdx = i;
+        }
+
+        return targetIdx == 0 ? true : false;
+    }
+};
