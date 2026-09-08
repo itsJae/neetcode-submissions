95b2eb1c70241a6271962f7f255af700c84cb3b7
Lim Woojae <limwoojae.dev@gmail.com>
2026-09-08 14:13:20 +1000

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int l = 0;
+        int r = 0;
+        int n = nums.size();
+        int ans = 0;
+
+        while (l < n - 1 && r < n - 1) {
+            cout << "l: " << l << ", r: " << r << endl; 
+            int maxReachable = 0;
+
+            for (int i = l; r < n && i <= r; ++i) {
+                if (i + nums[i] > maxReachable) {
+                    maxReachable = i + nums[i];
+                }
+            }
+
+            ++ans;
+            l = r + 1; 
+            r = maxReachable;
+        }    
+
+        return ans;
+    }
+};
