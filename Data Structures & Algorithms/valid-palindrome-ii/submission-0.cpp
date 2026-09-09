ef47dac8667f74919b56d8b2bf450e321c46874c
Lim Woojae <limwoojae.dev@gmail.com>
2026-09-09 23:00:02 +1000

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0;
+        int r = s.size() - 1;
+
+        while (l < r) {
+            if (s[l] != s[r])
+                return false;
+            ++l;
+            --r;
+        }
+
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        string tmp = s;
+
+        for (int i = 0; i < s.size(); ++i) {
+            tmp.erase(i, 1);
+            // cout << "tmp: " << tmp << endl;
+            
+            if (isPalindrome(tmp)) 
+                return true;
+            
+
+            tmp = s;
+        }
+
+        return false;
+    }
+};
