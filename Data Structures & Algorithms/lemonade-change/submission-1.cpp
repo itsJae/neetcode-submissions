861c29169326fbd49256e2b1056b99ac894608c0
Lim Woojae <limwoojae.dev@gmail.com>
2026-09-09 13:18:50 +1000

    Add: lemonade-change - submission-1

---
+++ Data Structures & Algorithms/lemonade-change/submission-1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        if (bills[0] > 5) return false;
+        
+        int n = bills.size();
+        unordered_map<int, int> m;
+
+        for (int i = 0; i < n; ++i) {
+            cout << i << endl;
+            if (bills[i] == 5) {
+                ++m[bills[i]];
+            } else if (bills[i] == 10) {
+                if (m[5] == 0) return false;
+                --m[5];
+                ++m[10];
+            } else {
+                if (m[5] > 0 && m[10] > 0) {
+                    --m[5];
+                    --m[10];
+                } else if (m[5] > 2 && m[10] == 0) {
+                    m[5] -= 3;
+                } else
+                    return false;
+
+                ++m[20];
+            }
+        }
+
+        return true;
+    }
+};
