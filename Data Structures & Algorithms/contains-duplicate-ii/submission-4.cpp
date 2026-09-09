5241589bb08cf6746f1d90546c0cc1f12986305a
Lim Woojae <limwoojae.dev@gmail.com>
2026-09-10 00:26:24 +1000

    Add: contains-duplicate-ii - submission-4

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-4.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int, int> m;
+        
+        // setup window
+        for (int i = 0; i < n && i <= k; ++i) {
+            ++m[nums[i]];
+        }
+
+        int i = 0;
+        while (i < n) {
+            int curr = nums[i];
+
+            if (m[curr] >= 2) 
+                return true;  
+
+            if (k + 1 < n) {
+                ++k;
+                ++m[nums[k]];
+            }
+
+            ++i;
+            --m[curr];
+        }
+
+        return false;
+    }
+};
