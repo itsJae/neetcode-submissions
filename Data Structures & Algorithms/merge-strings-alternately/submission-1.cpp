95bb664d5c4e92443f830829a8befd085f3b4f4e
Lim Woojae <limwoojae.dev@gmail.com>
2026-09-09 23:10:25 +1000

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans = "";
+        int p1 = 0;
+        int p2 = 0;
+        int s1 = word1.size();
+        int s2 = word2.size();
+
+        while (p1 < s1 && p2 < s2) {
+            ans.push_back(word1[p1]); 
+            ans.push_back(word2[p2]); 
+
+            ++p1;
+            ++p2;
+        } 
+
+        // cout << p1 << ", " << p2 << endl;
+
+        if (p1 == s1) {
+            while (p2 < s2) {
+                ans.push_back(word2[p2]);
+                ++p2;
+            }
+        } else if (p2 == s2) {
+            while (p1 < s1) {
+                ans.push_back(word1[p1]);
+                ++p1;
+            }
+        }
+
+        return ans;
+    }
+};
