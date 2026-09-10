e50a86f1cf879c5552a965c40756cbcbe9a63bc8
Lim Woojae <limwoojae.dev@gmail.com>
2026-09-10 11:24:32 +1000

    Add: find-k-closest-elements - submission-2

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        vector<int> ans;
+        int l = 0;
+        int r = k;
+
+        while (r < arr.size()) {
+            if (abs(arr[r] - x) > abs(arr[l] - x) || 
+                (abs(arr[r] - x) == abs(arr[l] - x) && arr[r] > arr[l])
+            ) {
+                break;
+            }
+
+            ++l;
+            ++r;
+        }
+
+        for (int i = l; i < r && i < arr.size(); ++i) 
+            ans.push_back(arr[i]);
+        
+        return ans;
+    }
+};
