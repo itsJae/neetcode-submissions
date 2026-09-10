e257747928f0ee36af3a4e346f197e3d5a64eba1
Lim Woojae <limwoojae.dev@gmail.com>
2026-09-10 17:21:23 +1000

    Add: capacity-to-ship-packages-within-d-days - submission-0

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-0.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        long long l = *max_element(weights.begin(), weights.end());
+        long long r = accumulate(weights.begin(), weights.end(), 0);
+        int ans = INT_MAX;
+
+        while (l <= r) {
+            int cnt = 0;
+            int sum = 0;
+            int mid = (l + r) / 2;
+            int i = 0;
+
+            while (i < weights.size()) {
+                sum += weights[i];
+                // cout << "sum: " << sum << endl;
+
+                if (sum > mid) {
+                    ++cnt;
+                    sum = 0;
+                    continue;
+                }
+
+                ++i;
+            }
+
+            if (sum <= mid) 
+                ++cnt;
+
+            // cout << "mid: " << mid << ", cnt: " << cnt << endl;
+
+            if (cnt <= days) {
+                ans = min(ans, mid);
+                r = mid - 1;
+            } else if (cnt > days) {
+                l = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
