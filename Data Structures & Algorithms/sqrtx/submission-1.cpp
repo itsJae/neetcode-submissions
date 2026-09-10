cb98e64bdf3de2de3b5cc61ebc99e73490dcd5a3
Lim Woojae <limwoojae.dev@gmail.com>
2026-09-10 15:48:20 +1000

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long l = 0;
+        long long r = x;
+        long long maxVal = 0;
+
+        while (l <= r) {
+            long long mid = (l + r) / 2;
+            long long sqr = mid * mid;
+            // cout << "mid: " << mid << endl;
+
+            if (sqr == x) {
+                return mid;
+            } else if (sqr > x) {
+                r = mid - 1;
+            } else {
+                if (sqr > maxVal) {
+                    maxVal = mid;
+                }
+
+                l = mid + 1;
+            }
+        }
+
+        return maxVal;
+    }
+};
