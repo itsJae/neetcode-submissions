c7dd0be5e8c41fe8fa349e057c7416974e5053c9
Lim Woojae <limwoojae.dev@gmail.com>
2026-09-10 15:19:53 +1000

    Add: asteroid-collision - submission-2

---
+++ Data Structures & Algorithms/asteroid-collision/submission-2.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> ans;
+        stack<int> st;
+
+        st.push(asteroids[0]);
+
+        for (int i = 1; i < asteroids.size(); ++i) {
+            int curr = asteroids[i];
+
+            if (st.empty() || (curr < 0 && st.top() < 0) || curr > 0) {
+                st.push(curr);
+                continue;
+            }
+
+            cout << "collision" << endl;
+
+            while (true) {
+                if (st.empty()) {
+                    st.push(curr);
+                    break;
+                }
+
+                int top = st.top();
+
+                if ((top < 0 && curr < 0) || (top > 0 && curr > 0)) {
+                    st.push(curr);
+                    break;
+                }
+
+                if (top == abs(curr)) {
+                    st.pop();
+                    break;
+                } else if (top < abs(curr)) {
+                    st.pop();
+                } else {
+                    break;
+                }
+            }
+        }
+
+        while (!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
